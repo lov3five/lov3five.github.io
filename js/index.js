5477bcaedfc23a3c2061af44da9599e2022c2692
lov3five <luongtranstaff@gmail.com>
2022-10-08 15:05:50 +0700

    Update

--- js/index.js
+++ js/index.js
@@ -2,3 +2,15 @@ function showWarning(){
     console.log("Downloading...!");
 }
 
+let a = '{"name":"John", "age":30, "car":null}';
+
+function convertJSONToJS(input){
+    return JSON.parse(input);
+}
+
+function convertJSToJSON(input){
+    return JSON.stringify(input);
+}
+
+console.log(convertJSONToJS(a));
+console.log(convertJSToJSON(convertJSONToJS(a)));
